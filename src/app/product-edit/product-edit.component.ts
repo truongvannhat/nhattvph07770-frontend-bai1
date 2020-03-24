3706b7f6ebc5b700eb2e2b4ce823e70182b6ecc5
truongvannhat <truongvannhat200x@gmail.com>
2020-03-24 13:03:08 +0700

    1 file modified, 1 added

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../Product';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
-
-  constructor() { }
+  product: Product;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this.getProduct();
   }
 
+  getProduct(){
+    this.route.params.subscribe(param => {
+        this.productService.getProduct(param.productID).subscribe(data => this.product = data);
+    })
+  }
+  updateProduct(){
+       this.productService.updateProduct(this.product).subscribe(data => console.log(data));
+  }
 }
